bb6f426ad12360fcc82526e9aa246d05751cdc3b
Todd Matthews <todd@heroku.com>
2010-12-06 14:49:25 -0800

    sty-lish

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
 
+gem 'haml'
 gem 'sinatra'
 gem 'thin'
--- main.rb
+++ main.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 
-set :sass, {:style => :compact, :load_paths => [File.join(Sinatra::Application.views, 'stylesheets')]}
-
 not_found do
   headers["Status"] = "301 Moved Permanently"
   redirect("/")
@@ -12,6 +10,6 @@ get '/' do
   erb :index
 end
 
-get '/stylesheets/screen.css' do
+get '/screen.css' do
   sass :screen
 end
